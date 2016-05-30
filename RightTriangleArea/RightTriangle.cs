60db0c2ef95ca1ca8f28144051e09fe24321a165
Yury Cooperman <yurycooperman@gmail.com>
2016-05-30 04:41:41 +0300

    Ничего нового

---
+++ RightTriangleArea/RightTriangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangleArea
+{
+    public class RightTriangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public const string NegativeSideErrorMessage = "Sides cannot be negative";
+        public const string PythagoreanErrorMessage = "Triangle is not right";
+
+        private RightTriangle()
+        {
+
+        }
+
+        public RightTriangle(double side1, double side2, double side3)
+        {
+            double[] sides = { side1, side2, side3 };
+            if (side1 < 0 || side2 < 0 || side3 < 0)
+            {
+                throw new ArgumentException(NegativeSideErrorMessage);
+            }
+            Array.Sort(sides);
+            if (sides[2]*sides[2]!=(sides[0]*sides[0]+sides[1]*sides[1]))//теорема Пифагора
+            {
+                throw new ArgumentException(PythagoreanErrorMessage);
+            }
+            a = sides[0];
+            b = sides[1];
+            c = sides[2];
+        }
+
+        public double Area()
+        {
+            double sp = (a + b + c) / 2;//полупериметр
+            return (sp - a) * (sp - b);//Формула Герона
+        }
+    }
+}
---
+++ TriangleTest/RightTriangleTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RightTriangleArea;
+
+namespace TriangleTest
+{
+    [TestClass]
+    public class RightTriangleTest
+    {
+        [TestMethod]
+        public void RightTriangle_NegativeSides_ArgumentException()//Проверка сторон на отрицательные значения
+        {
+            //arrange
+            double a = 1;
+            double b = -7;
+            double c = 3;
+
+            //act
+            try
+            {
+                RightTriangle TestTriangle = new RightTriangle(a, b, c);
+            }
+            catch (ArgumentException e)
+            {
+                //assert
+                StringAssert.Contains(e.Message, RightTriangle.NegativeSideErrorMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        [TestMethod]
+        public void RightTriangle_InvalidSides_ArgumentException()//Проверка треугольника на прямоугольность
+        {
+            //arrange
+            double a = 1;
+            double b = 7;
+            double c = 3;
+
+            //act
+            try
+            {
+                RightTriangle TestTriangle = new RightTriangle(a, b, c);
+            }
+            catch (ArgumentException e)
+            {
+                //assert
+                StringAssert.Contains(e.Message, RightTriangle.PythagoreanErrorMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        [TestMethod]
+        public void RightTriangle_CorrectInput_Area()//Проверка правильности расчета площади
+        {
+            // arrange
+            double a = 5;
+            double b = 3;
+            double c = 4;
+            double expected = 6;
+            RightTriangle TestTriangle = new RightTriangle(a, b, c);
+
+            // act
+            double area = TestTriangle.Area();
+
+            // assert
+            Assert.AreEqual(expected, area, 0.001, "Area is incorrect");
+        }
+    }
+}
